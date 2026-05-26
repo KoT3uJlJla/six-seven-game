0a07eabb2d7b1850a71dd208b572aaf692e5f45f
KoT3uJlJla <48926752+KoT3uJlJla@users.noreply.github.com>
2026-05-26 17:21:24 +0500

    worker: load final share guard last

--- _worker.js
+++ _worker.js
@@ -34,6 +34,9 @@ export default {
     if (!html.includes('release-story-share.js')) {
       html = html.replace('</body>', '  <script src="release-story-share.js"></script>\n</body>');
     }
+    if (!html.includes('release-share-final-fix.js')) {
+      html = html.replace('</body>', '  <script src="release-share-final-fix.js"></script>\n</body>');
+    }
 
     return new Response(html, {
       status: asset.status,
